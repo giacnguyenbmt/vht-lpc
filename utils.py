ce9984a78ea7cde62bfe2d43cea14200d762c266
Nguyen Pham <46295596+giacnguyenbmt@users.noreply.github.com>
2023-05-24 09:00:41 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -167,7 +167,7 @@ data_transform = A.Compose([
                             # A.MotionBlur(p=0.3, blur_limit=5),
                             
                             # A.RandomBrightnessContrast(p=1, brightness_limit=0.5, contrast_limit=0.8),
-                            A.ColorJitter(p=1, brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),
+                            A.ColorJitter(p=1, brightness=(0.8, 1.2), contrast=(0.8, 1.2), saturation=(0.8, 1.2), hue=0.05),
                             
                             # A.GaussNoise(p=0.5, var_limit=0.09),
                           ])
